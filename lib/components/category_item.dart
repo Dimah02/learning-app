6af660c5aa0476e449693c6be5d20bd3dafb1a79
Dimah <dimahmehdawi@gmail.com>
2024-05-10 12:52:41 +0300

    add categories ui

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,28 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Category extends StatelessWidget {
-  String? text;
-  Color? color;
-  VoidCallback? onTap;
-  Category({this.text, this.color, this.onTap});
+  final String? text;
+  final String? image;
+  final Color? color;
+  final VoidCallback? onTap;
+  const Category({super.key, this.text,this.image, this.color, this.onTap});
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        color: color,
-        width: double.infinity,
-        height: 65,
-        alignment: Alignment.centerLeft,
-        padding: EdgeInsets.only(left: 24),
-        child: Text(
-          '${text!}',
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-          ),
+        width: 110,
+        height: 110,
+        child: CircleAvatar(
+          backgroundImage: AssetImage(image!),
+          backgroundColor: color!,
+          child: Text(text!,style: const TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize: 20,backgroundColor: Color.fromARGB(86, 0, 0, 0),letterSpacing: 1.5),),
         ),
-      ),
-    );
+      ));
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_app/components/category_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,9 +8,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:const Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
+          title:
+              const Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
             Image(
               image: AssetImage("assets/images/logo/puzzle.png"),
               width: 35,
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
             )
           ]),
         ),
-        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        body: ListView(children: [
           const Padding(
             padding: EdgeInsets.only(left: 32.0),
             child: Text("Random words",
@@ -32,7 +32,8 @@ class HomePage extends StatelessWidget {
                 )),
           ),
           Container(
-            margin: const EdgeInsets.only(left: 32, right: 32, top: 16, bottom: 16),
+            margin:
+                const EdgeInsets.only(left: 32, right: 32, top: 16, bottom: 16),
             height: 250,
             width: double.infinity,
             color: Colors.red,
@@ -40,18 +41,61 @@ class HomePage extends StatelessWidget {
           Row(
             children: [
               const Spacer(flex: 1),
-              ElevatedButton(onPressed: () {}, child:const Text("left")),
+              ElevatedButton(onPressed: () {}, child: const Text("left")),
               const Spacer(flex: 1),
-              ElevatedButton(onPressed: () {}, child:const Text("right")),
+              ElevatedButton(onPressed: () {}, child: const Text("right")),
               const Spacer(flex: 1),
             ],
           ),
           const Padding(
-            padding: EdgeInsets.only(left: 32.0,top: 16),
+            padding: EdgeInsets.only(left: 32.0, top: 16, bottom: 16),
             child: Text("Categories",
                 style: TextStyle(
                   fontSize: 24,
                 )),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Category(
+                color: const Color(0XFF8078BC),
+                onTap: () {},
+                text: "Numbers",
+                image: "assets/images/categories/numbers.png",
+              ),
+              const SizedBox(
+                width: 32,
+              ),
+              Category(
+                color: const Color(0XFFF4DD45),
+                onTap: () {},
+                text: "Family",
+                image: "assets/images/categories/family.png",
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Category(
+                color: const Color(0XFF86F1A7),
+                onTap: () {},
+                text: "Colors",
+                image: "assets/images/categories/palette.png",
+              ),
+              const SizedBox(
+                width: 32,
+              ),
+              Category(
+                color: const Color(0XFFD82973),
+                onTap: () {},
+                text: "Animals",
+                image: "assets/images/categories/animals.png",
+              ),
+            ],
           ),
         ]));
   }
