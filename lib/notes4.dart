6ae32f05007b15a258333e9441e196c229f9e30d
Dimah Mehdawi <105394518+Dimah02@users.noreply.github.com>
2024-07-01 16:57:53 +0300

    Create notes4.dart

---
+++ lib/notes4.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather App'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              //   return const Text("yes");
+              // }));
+            },
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: const Text("yes"),
+    );
+  }
+}
